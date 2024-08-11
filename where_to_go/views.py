0187ab0514001f11cd6c3751b19aba1f82ebe287
Vladimir Kobzev <cello@DESKTOP-CO6EMTO>
2024-08-11 13:45:13 +0300

    feat: added render geojson places via view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
+from places.models import Place
 
 def show_start_page(request):
     template = loader.get_template('index.html')
-    context = {}
+    context = {'h4': 'Выберите место на карте2', 'title':'Куда пойти', 'places': Place.objects.all()}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
