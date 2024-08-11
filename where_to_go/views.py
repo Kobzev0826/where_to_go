648eebb33598e5ff5e3ff704553c1bbb9d7857bc
Vladimir Kobzev <cello@DESKTOP-CO6EMTO>
2024-08-11 14:11:49 +0300

    chore: fix render

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -4,6 +4,8 @@ from places.models import Place
 
 def show_start_page(request):
     template = loader.get_template('index.html')
-    context = {'h4': 'Выберите место на карте2', 'title':'Куда пойти', 'places': Place.objects.all()}
+    places =  Place.objects.all()
+
+    context = {'title':'Куда пойти', 'places': places}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
