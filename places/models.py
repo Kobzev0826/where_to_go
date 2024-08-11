9b809a2e44de07912533959998572af80d5ca9ab
Vladimir Kobzev <cello@DESKTOP-CO6EMTO>
2024-08-11 12:14:56 +0300

    feat: added place models and migrations for them

--- places/models.py
+++ places/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
-# Create your models here.
-print("Hello, models.py!")
+class Place(models.Model):
+    title = models.CharField('Title', max_length=100)
+    short_description = models.TextField('Short description', blank=True)
+    detail_description =  models.TextField("Detailed description", blank=True)
+    lat = models.FloatField('Latitude')
+    lon = models.FloatField('Longitude')
+
+    def __str__(self):
+        return self.title
