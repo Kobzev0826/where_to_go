3ef0be272789cc40521b8b5bebba84714b049c7a
Vladimir Kobzev <cello@DESKTOP-CO6EMTO>
2024-10-14 16:13:10 +0300

    feat: added WYSIWYG redactor

--- places/models.py
+++ places/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
-
+from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Title', max_length=100)
     short_description = models.TextField('Short description', blank=True)
-    detail_description =  models.TextField("Detailed description", blank=True)
+    detail_description =  HTMLField("Detailed description", blank=True)
     lat = models.FloatField('Latitude')
     lon = models.FloatField('Longitude')
 
