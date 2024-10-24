986ac9a8a8e03d42376a760594b6be54a777ecd5
Vladimir Kobzev <cello@DESKTOP-CO6EMTO>
2024-10-24 21:35:51 +0300

    feat: use '' everywhere

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -5,23 +5,24 @@ from places.models import Place
 from django.http import HttpResponseNotFound, JsonResponse
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
+
 def show_start_page(request):
     template = loader.get_template('index.html')
     places = []
     for place in Place.objects.all():
         places.append({
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.lon, place.lat]
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lon, place.lat]
             },
-            "properties": {
-                "title": place.title,
-                "placeId": place.id,
-                "detailsUrl": f"place/{place.id}"
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': f'place/{place.id}'
             }
         })
-    context = {'title':'Куда пойти',  'places': {"type": "FeatureCollection","features": places}}
+    context = {'title': 'Куда пойти', 'places': {'type': 'FeatureCollection', 'features': places}}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
 
@@ -30,15 +31,14 @@ def show_place(request, place_id):
     requested_place = get_object_or_404(Place, id=place_id)
     images = [image.image.url for image in requested_place.images.all()]
 
-
     place = {
         'title': requested_place.title,
         'imgs': images,
-        "description_short": requested_place.short_description,
-        "description_long": requested_place.detail_description,
+        'description_short': requested_place.short_description,
+        'description_long': requested_place.detail_description,
         'coordinates': {
-            "lat": requested_place.lat,
-            "lng": requested_place.lon
+            'lat': requested_place.lat,
+            'lng': requested_place.lon
         }
     }
     return JsonResponse(place)
