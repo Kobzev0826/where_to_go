0058d57bc502e3701f60907c6b22094e08a4f7a2
Vladimir Kobzev <cello@DESKTOP-CO6EMTO>
2024-10-24 20:59:16 +0300

    feat: remove default value for secret_key

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = Env()
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "REPLACE_ME")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
