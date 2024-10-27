9fd2f41bb9392b5c0e9f9bfa4481f37255d37704
Vladimir Kobzev <cello@DESKTOP-CO6EMTO>
2024-10-27 12:18:13 +0300

    added Image to admin panel

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 from places.models import Place, Image
 
+
 class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model=Image
     readonly_fields = ['image_preview', ]
@@ -14,3 +15,8 @@ class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    model = Image
+    raw_id_fields = ("place",)
